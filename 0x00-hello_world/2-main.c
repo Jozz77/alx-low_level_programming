dca13c0aa4cc01061c77ecc9ebff9e8ea0e1fb61
Jonathan Mmadu <mmadujonathan@gmail.com>
2022-09-08 21:23:56 -0700

    update

--- 0x00-hello_world/2-main.c
+++ 0x00-hello_world/2-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always (Success)
- */
+* main - Entry point
+*
+* Return: Always (Success)
+*/
 int main(void)
 {
-  printf("Holberton School");
-  return (0);
+return (0);
 }
