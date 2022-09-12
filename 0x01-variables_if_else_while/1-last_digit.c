f250be18932f25210f4ac7f41a4fda5780998c5a
Jonathan Mmadu <mmadujonathan@gmail.com>
2022-09-11 19:09:22 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's descricption'
+*
+* Return: Always 0 (Success)
+*/
 
 /* betty style doc for function main goes there */
 int main(void)
