1cb158595b52f5fb7243bb408dc4583a3c45ca5b
Jonathan Mmadu <mmadujonathan@gmail.com>
2022-09-18 11:40:51 -0700

    debug a function

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+int n;
+ n = 98;
+ printf('n')
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is the basic of debugging in C
